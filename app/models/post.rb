583315f8154531684199350cd8b5c3866009ec32
carly <carly@noukod.com>
2019-02-01 19:25:45 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
   #put new code here
@@ -17,4 +17,10 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+
+  def self.by_author(author_id)
+  where(author: author_id)
+end
+
+
 end
